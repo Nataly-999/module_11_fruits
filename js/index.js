9269394329683d0ba316f695c1fbaaec5da53c42
Natalya <npm999@mail.ru>
2024-10-28 18:41:07 +0300

    add quickort

--- js/index.js
+++ js/index.js
@@ -165,7 +165,7 @@ const comparationColor = (a, b) => {
   return priority1 > priority2;
 };
 
-  // функция обмена элементов
+ /* // функция обмена элементов
 function swap(items, firstIndex, secondIndex){
   const temp = items[firstIndex];
   items[firstIndex] = items[secondIndex];
@@ -191,7 +191,7 @@ function swap(items, firstIndex, secondIndex){
         }
     }
     return i;
-  }
+  }*/
 
 const sortAPI = {
   bubbleSort(fruits, comparationColor) {
@@ -212,8 +212,8 @@ const sortAPI = {
     } 
   },
 
-// алгоритм быстрой сортировки
-  quickSort(fruits, comparationColor) {
+/*// алгоритм быстрой сортировки
+  quickSort(items, left, right) {
     // TODO: допишите функцию быстрой сортировки
     var index;
     if (items.length > 1) {
@@ -228,7 +228,31 @@ const sortAPI = {
         }
     }
     return items;
-  },
+  },*/
+
+
+// алгоритм быстрой сортировки
+ quickSort (arr) {
+  // Условие остановки, выхода из рекурсии, возвращем массив с 1 элементом
+  if (arr.length < 2) return arr;
+  // Выбираем опорный элемент
+  let pivot = arr[0];
+  // Определяем массивы для тех, что меньше и больше опорного
+  const left = [];
+  const right = [];
+
+  // Проходим циклом по всем элементам из массива и разносим их в массивы созданные ранее согласно условию, больше опорного - в правый, меньше - в левый  
+  for (let i = 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  // Рекурсивно повторяем процесс для новых двух массивов, текущий опорный элемент - кладем как первый в правый массив.
+  return quickSort(left).concat(pivot, quickSort(right));
+},
+
 
   // выполняет сортировку и производит замер времени
   startSort(sort, fruits, comparationColor) {
