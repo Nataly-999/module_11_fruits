bb1145ba5f30f24e12287fa1213acc291c0ef355
Natalya <npm999@mail.ru>
2024-10-29 17:21:51 +0300

    update quickort with comparationolor

--- js/index.js
+++ js/index.js
@@ -237,14 +237,14 @@ const sortAPI = {
   // Условие остановки, выхода из рекурсии, возвращем массив с 1 элементом
   if (arr.length < 2) return arr;
   // Выбираем опорный элемент
-  let pivot = arr[parseInt(arr.length-1)/2];
+  let pivot = arr[0];
   // Определяем массивы для тех, что меньше и больше опорного
   const left = [];
   const right = [];
 
   // Проходим циклом по всем элементам из массива и разносим их в массивы созданные ранее согласно условию, больше опорного - в правый, меньше - в левый  
   for (let i = 1; i < arr.length; i++) {
-    if (pivot > arr[i]) {
+    if (comparationColor(pivot, arr[i])) {
       left.push(arr[i]);
     } else {
       right.push(arr[i]);
@@ -252,7 +252,8 @@ const sortAPI = {
   }
   // Рекурсивно повторяем процесс для новых двух массивов, текущий опорный элемент - кладем как первый в правый массив.
   //return quickSort(left).concat(pivot, quickSort(right)); 
-  return [...sortAPI.quickSort(left), pivot, ...sortAPI.quickSort(right)]; 
+  return [...sortAPI.quickSort(left), pivot, ...sortAPI.quickSort(right)];
+  //return [...quickSort(left), pivot, ...quickSort(right)];  
   },
 
 
